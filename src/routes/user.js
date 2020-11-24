30e08835da3902154a91c3f4203f5b132f5442f9
gonzalozevallos <gonzalo.zev@gmail.com>
2020-11-24 14:50:28 -0300

    add multer

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 
 const validator = require('../middlewares/validator');
 
 const userController = require('../controllers/userController');
 
+// Multer config
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images/users');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + ' - ' + file.originalname);
+    }
+})
+
+var upload = multer({ storage: storage })
+
 // Muestra la vista de registro
 router.get('/register', userController.showRegister);
 
 // Procesa la vista de registro
-router.post('/register', validator.register, userController.processRegister);
+router.post('/register', upload.any(), validator.register, userController.processRegister);
 
 // Muestra la vista de login
 router.get('/login', userController.showRegister);
