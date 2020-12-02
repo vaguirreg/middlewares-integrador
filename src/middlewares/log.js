dbc7a446a8428a7441f8a83c3b0d69cebbe92120
gonzalo <gonzalo.zev@gmail.com>
2020-12-02 17:35:26 -0300

    refactor log

--- src/middlewares/log.js
+++ src/middlewares/log.js
@@ -1,19 +1,10 @@
 const readJson = require('../helpers/readJson');
 
 module.exports = (req, res, next) => {
-    
-    if(req.session.user){
-        return next();
-    } else if (req.cookies.user){
+    if (!req.session.user && req.cookies.user){
         const allUsers = readJson();
         const userFound = allUsers.find(user => user.id == req.cookies.user);
-        if(!userFound){
-            console.log('Cookie creada pero el usuario no se encontró con ese id');
-        }
         req.session.user = userFound;
-        return next();
-    } else {
-        return next();
     }
-
+    return next();
 }
