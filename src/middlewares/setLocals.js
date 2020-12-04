0327681b80abbe02d59a07ecb17862961792562d
gonzalo <gonzalo.zev@gmail.com>
2020-12-04 11:30:59 -0300

    minor refactor setLocals

--- src/middlewares/setLocals.js
+++ src/middlewares/setLocals.js
@@ -1,10 +1,9 @@
 module.exports = (req, res, next) => {
-
     res.locals.userLog = false;
 
     if (req.session.user) {
         res.locals.userLog = req.session.user;
     }
-
+    
     return next();
 }
